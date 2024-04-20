1a2580d12fd51314e1b8eb5401582a73c5989e70
Alexis Almeida <alexis.cesar2017@outlook.com>
2024-04-20 20:19:29 -0300

    bkp

--- jobs.ps1
+++ jobs.ps1
@@ -6,3 +6,5 @@ $computers | % { Start-Job { Write-Host "test in $_" } }
 # Requires RSAT
 # To find windows feature name: Get-WindowsFeature -ComputerName nameOfComputer (name property)
 # $computers | % { Start-Job { Install-WindowsFeature -Name Web-WebSocket -ComputerName $_ } }
+
+Get-Job
