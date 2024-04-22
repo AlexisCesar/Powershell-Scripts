7a0463c5eb20d51d9785fa8543983fda0e5e54f6
Alexis Cesar <alexis.cesar2017@outlook.com>
2024-04-22 14:31:45 -0300

    bkp

--- remotesession.ps1
+++ remotesession.ps1
@@ -1,4 +1,10 @@
 # Used to run applications in the remote computer, if trying to execute an application in the local session pointing to a remote path, it will try to run locally.
-Enter-PSSession -ComputerName SRV01
-
+Enter-PSSession -ComputerName SRV01 
 Exit-PSSession
+#only works in interactive ps session, for example, typing the command in the console
+# if trying to access a remote ps session within a script, NewPSSession should be used like this
+#
+$session = New-PSSession -ComputerName SRV01
+
+#running commands in this session:
+Invoke-Command -Session $session -ArgumentsList (...) -ScriptBlock { ...$($args[0])... }
