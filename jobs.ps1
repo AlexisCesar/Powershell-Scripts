eb1146c388dc30cf67d90d2f3e610b5e17061bf0
Alexis Cesar <alexis.cesar2017@outlook.com>
2024-04-22 13:44:54 -0300

    bkp jobs

--- jobs.ps1
+++ jobs.ps1
@@ -1,10 +1,37 @@
  
-$computers = 1..7 | % { "COMPUTER_SRV_0$_" };
+$computers = 1..7 | ForEach-Object { "SRV_0$_" };
 
-$computers | % { Start-Job { Write-Host "test in $_" } }
+
+$jobScriptBlock = {
+    param(
+        [String] $computerName
+    )
+
+    Write-Host "Installing feature in $computerName"
+}
+
+
+$computers | ForEach-Object { 
+    Start-Job -Name "JOB $_" -ScriptBlock $jobScriptBlock -ArgumentList ($_)
+}
 
 # Requires RSAT
 # To find windows feature name: Get-WindowsFeature -ComputerName nameOfComputer (name property)
 # $computers | % { Start-Job { Install-WindowsFeature -Name Web-WebSocket -ComputerName $_ } }
 
-Get-Job
+# Get-Job
+
+# (Get-Job)[-1] | Format-List
+
+# $jobReturn = Receive-Job (Get-Job)[-1].Name # Receive-Job removes the value from memory after being read, so it should be
+# stored in a variable, or, use it like this:
+# Receive-Job ((Get-Job)[-1].Name) -Keep
+
+# it is possible to get all jobs with Receive-Job (Get-Job)
+
+# to clear jobs in session Remove-Job (Get-Job)
+
+<#
+also it's possible to use args without a scriptblock declaring params, like this
+Start-Job -ArgumentList ($_) -ScriptBlock { Write-Host "Running in $args[0]" }
+#>
